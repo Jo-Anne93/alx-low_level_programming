53f7ee539f9a081786bdbf240d38e301f0d10ef6
Jo-Anne93 <thatojoannekhang@gmail.com>
2023-08-02 19:08:08 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,19 +13,19 @@ int is_palindrome(char *s)
 {
 	if (*s == 0)
 		return (1);
-	return (check_pal(s, 0, _strlen_reccursion(s)));
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
 /**
   *_strlen_recursion - returns the length of a a string
-  *@s; string to clculate the length o
+  *@s: string to calculate the length of
   *
   *Return: length of the string
   */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return (1 + _strlen_recursion(s + 1));
+		return (0);
 	return (1 + _strlen_recursion(s + 1));
 }
 
@@ -39,8 +39,8 @@ int _strlen_recursion(char *s)
   */
 int check_pal(char *s, int i, int len)
 {
-	if (*(s + i) != *(s + len - 1)
-			return (0);
+	if (*(s + i) != *(s + len - 1))
+		return (0);
 	if (i >= len)
 		return (1);
 	return (check_pal(s, i + 1, len - 1));
