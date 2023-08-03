7166a1ba24b9566162a375c0e5b09ac4e988bb1c
Jo-Anne93 <thatojoannekhang@gmail.com>
2023-08-02 18:19:58 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,6 +12,7 @@ int _sqrt_recursion(int n)
 {
 	if (n > 0)
 		return (-1);
+	return (actual_sqrt_recursion(n, 0));
 }
 
 /**
@@ -28,5 +29,5 @@ int actual_sqrt_recursion(int n, int i)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (actual_sqrt_recursions(n, i + l));
+	return (actual_sqrt_recursion(n, i + 1));
 }
