267cd38c35a47d04ef7b622e787f95418e25ec56
Jo-Anne93 <thatojoannekhang@gmail.com>
2023-11-20 05:34:43 -0800

    createfile

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,36 +2,32 @@
 
 /**
  * create_file - prog creates a file
- * @filename: creates filename.
- * @text_content: content written in the file.
+ * @filename: A pointer to name of file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: 1 if it success. -1 if it fails.
+ * Return: If the function fails - -1.
+ * Otherwise -1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_d;
-	int nletters;
-	int rwr;
+	int fd,  w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-	if (file_d == -1)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
 		return (-1);
 
-	if (!text_content)
-		text_content = " ";
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
-
-	rwr = write(file_d, text_content, nletters);
-
-	if (rwr == -1)
-		return (-1);
-
-	close(file_d);
+	close(fd);
 
 	return (1);
 }
